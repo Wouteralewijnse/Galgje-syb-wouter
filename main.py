52a815fefe95972ea27a44408e92fd8de4cda32c
WouterAlewijnse <74767434+Wouteralewijnse@users.noreply.github.com>
2021-06-14 14:08:54 +0000

    Woordenlijst toegevoegd

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ print("Voer uw naam in alstublieft:")
 naam = input()
 print("Hallo " + naam + "!")
 
+counter = 0
+woordenlijst = ["informatica", "informatiekunde", "spelletje", "aardigheidje", "scholier", "fotografie", "waardebepaling", "specialiteit", "verzekering", "universiteit", "heesterperk" ]
+
+
