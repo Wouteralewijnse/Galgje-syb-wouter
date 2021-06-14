05307622c7687ccd43089becc1427d99349de331
WouterAlewijnse <74767434+Wouteralewijnse@users.noreply.github.com>
2021-06-14 14:10:27 +0000

    Commentaar - begin toegevoegd, time sleeps, lengtewoord aangeven, lengte woorkeuze toegevoegd

--- main.py
+++ main.py
@@ -11,3 +11,26 @@ counter = 0
 woordenlijst = ["informatica", "informatiekunde", "spelletje", "aardigheidje", "scholier", "fotografie", "waardebepaling", "specialiteit", "verzekering", "universiteit", "heesterperk" ]
 
 
+woordkeuze = random.choice(woordenlijst)
+lengtewoord = len(woordkeuze)
+temp = "." * lengtewoord
+
+print("We gaan vandaag galgje spelen")
+
+time.sleep(2)
+
+print("Ben je klaar om de strijd aan te gaan?")
+
+time.sleep(2)
+
+print()
+print("Je hebt 5 levens bij elke fout geraden letter gaat er een leven af")
+
+time.sleep(2)
+
+print('Zijn je 5 levens op zonder het woord te raden dan heb je verloren')
+
+time.sleep(2)
+
+print("Het woord heeft " + str(lengtewoord) + " letters")
+
